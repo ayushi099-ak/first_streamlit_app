f5df9dca289e290a08b9b4f641926791ef10f675
Ayushi Kala <107436736+ayushi099-ak@users.noreply.github.com>
2022-06-14 02:15:31 +0530

    Update streamlit_app.py
    Added header
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title("My very first app")
+streamlit.header("This is going to be interesting")
+streamlit.text("This app would be in python")
+streamlit.text("yesss, letss starttt")
