af039bce58cebf1aaec0b66387ddca76cbd6eb63
Ayushi Kala <107436736+ayushi099-ak@users.noreply.github.com>
2022-07-01 00:30:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.header("The fuit load list contains:")
 streamlit.dataframe(my_data_row)
 
